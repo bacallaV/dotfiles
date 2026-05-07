b519d2abcf8411591f31bd0a98e05f37893e1547
bacallaV <valdohalo3@hotmail.com>
2026-05-07 16:53:58 -0600

    nvim: commented show hidden and ignored by default

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,13 +3,22 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        -- hidden = true,
+        -- ignored = true,
+
         sources = {
           explorer = {
+            -- hidden = true,
+            -- ignored = true,
             layout = {
               -- ojo: "layout.layout.position"
               layout = { position = "left" },
             },
           },
+          -- files = {
+          --   hidden = true,
+          --   ignored = true,
+          -- },
         },
       },
     },
